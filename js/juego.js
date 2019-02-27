c5a3b7aaca81ff1bd5c1cf744cbcb9f643828400
Diego Correa <diegoco66@gmail.com>
2019-02-26 22:52:48 -0500

    feat: Se agrega función para guardar puzzle escogido

--- js/juego.js
+++ js/juego.js
@@ -2,3 +2,7 @@ if (!window.localStorage.getItem('session')) {
     window.localStorage.setItem('session', 1);
     window.localStorage.setItem('Juegos', 0);
 }
+
+function escogePuzzle(nombrePuzzle) {
+    window.localStorage.setItem('escogido', nombrePuzzle);
+}
