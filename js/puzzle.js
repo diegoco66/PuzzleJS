2394e4efb5295b7d22d1dd54cad1b4e2b7eacfea
Diego Correa <diegoco66@gmail.com>
2019-03-04 20:49:45 -0500

    feat: Se agrega cronometro al tablero

--- js/puzzle.js
+++ js/puzzle.js
@@ -6,16 +6,21 @@ var posiciones = new Array(16).fill(0).map(() => {
 
     return removed[0];
 });
+var miTablero;
+const CRONOMETRO = document.getElementById('cronometro');
 
 class Puzzle {
 
     constructor(tablero) {
+        this.mostrarTiempo = this.mostrarTiempo.bind(this);
         this.tablero = tablero;
         this.inicializar();
+        this.time = '00:00:00';
     }
 
     inicializar() {
         this.construirTablero();
+        setInterval(this.mostrarTiempo, 1000);
     }
 
     /**
@@ -29,6 +34,31 @@ class Puzzle {
             fichas[i].setAttribute("src", "../img/" + this.tablero + "/pieza" + posiciones[i] + ".png");
         }
     }
+
+    /**
+     * Aumentar y mostrar tiempo de cronometro
+     */
+    mostrarTiempo() {
+        let tiempo = this.time.split(":");
+        let horas = parseInt(tiempo[0]);
+        let minutos = parseInt(tiempo[1]);
+        let segundos = parseInt(tiempo[2]);
+
+        segundos++;
+        if (segundos === 60) {
+            segundos = 0;
+            minutos++;
+        }
+        if (minutos === 60) {
+            minutos = 0;
+            horas++;
+        }
+
+        let nuevoTiempo = `${(horas < 10) ? '0' + horas : horas}:${(minutos < 10) ? '0' + minutos : minutos}:${(segundos < 10) ? '0' + segundos : segundos }`;
+
+        CRONOMETRO.innerHTML = nuevoTiempo;
+        this.time = nuevoTiempo;
+    }
 }
 
 /**
@@ -37,7 +67,7 @@ class Puzzle {
  * @param {string} nombreTablero 
  */
 function comenzarPuzzle(nombreTablero) {
-    var miTablero = new Puzzle(nombreTablero);
+    miTablero = new Puzzle(nombreTablero);
 }
 
 comenzarPuzzle(window.localStorage.getItem("escogido"));
